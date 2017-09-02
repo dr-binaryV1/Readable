8884b0a90d4328cb54dcb84e23da3710b519f75c
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 18:28:51 -0500

    feat: Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a className="navbar-brand" href="#">Readable</a>
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Categories</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
