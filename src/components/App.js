04ca38bdee0431db97c6823346be966810dc7865
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 18:29:16 -0500

    fix: Add header to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import { connect } from 'react-redux';
+
+import Header from './Header';
+import * as actions from '../actions';
 import '../App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+    this.props.getCategories();
+  } 
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
