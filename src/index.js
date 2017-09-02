65256c1dcea418c92a5b94af12d2dd380c922d7d
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 18:30:24 -0500

    fix: Update store to use reduxThunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 import './index.css';
+import reducer from '../src/reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(ReduxThunk)
+  )
+);
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import postData from './postsReducer';
+
+export default combineReducers({
+  postData
+});
