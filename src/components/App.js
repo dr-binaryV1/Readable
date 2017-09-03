bf15728300556cd5963386b849e91edb3aa4fa4b
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 22:26:17 -0500

    Add Route for category and root page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
 
 import PostsList from './PostsList';
+import CategoryList from './CategoryList';
 import Header from './Header';
 import * as actions from '../actions';
 import '../App.css';
@@ -16,10 +18,21 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <PostsList />
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <Route exact path="/" component={PostsList} />
+              <Route exact path="/:category" render={({ match }) => {
+                const { category } = match.params;
+                return <PostsList category={category} />
+              }} />
+            </div>
+            <div className="col-4"><CategoryList /></div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(null, actions)(App);
+export default withRouter(connect(null, actions)(App));
