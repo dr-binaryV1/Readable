c61a1691c100246e09c2547cbab4eccb772f192b
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 22:26:44 -0500

    chore: Clean up

---
+++ src/components/Category.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+
+function Category(props) {
+  function categoryClicked() {
+    props.history.push(`/${props.category.path}`)
+  }
+
+  return (
+    <div onClick={categoryClicked} className="card" id='card_id'>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{props.category.name}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default withRouter(Category);
---
+++ src/components/CategoryList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Category from './Category';
+
+function CategoryList(props) {
+    const { categories } = props;
+
+  return (
+    <div>
+      <div>
+        <h3>Categories</h3>
+        <hr />
+        {categories ? categories.map((category) => 
+          <Category key={category.name} category={category} />) : ''}
+      </div>
+    </div>
+  );  
+}
+
+function mapStateToProps(state) {
+  return {
+    categories: state.postData.categories
+  }
+}
+
+export default connect(mapStateToProps)(CategoryList);
