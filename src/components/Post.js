9ad36333c62790ef05c0b0021ab97db5fd49e1a3
dr-binaryV1 <developerdamian@gmail.com>
2017-09-02 22:25:38 -0500

    feat: Add edit and delete buttons as well as author

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 export default function Post(props) {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="card text-left">
-            <div className="card-body">
-              <h4 className="card-title">{props.post.title}</h4>
-              <p className="card-text">{props.post.body}</p>
-              <a href="#" className="btn btn-primary">Button</a>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          Column
-        </div>
+    <div className="card text-left">
+      <div className="card-body">
+        <h4 className="card-title">{props.post.title}</h4>
+        <p className="card-text">{props.post.body}</p>
+
+        <a href="#" className="card-text">comments(0)</a>
+        <br />
+        
+        <hr />
+        <i><p className="card-text">Author: {props.post.author}</p></i>
+        
+        <a href="#" className="btn btn-primary">Edit</a>
+        <a href="#" className="btn btn-danger">Delete</a>
       </div>
     </div>
   )
