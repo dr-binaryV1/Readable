f43e4a123a53b18e6eba5d370149ce362406f44a
damian.wynter <damian.wynter@particularpresence.com>
2017-09-04 14:29:47 -0500

    feat: Toggle EditPost component when edit button clicked

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import EditPost from './EditPost';
 
 import Vote from './Vote';
 
@@ -11,35 +12,40 @@ class Post extends Component {
   }
 
   render() {
-    const {post, history} = this.props;
+    const { post } = this.props;
     const date = new Date().toDateString(post.timestamp);
 
     return (
-      <div className="post-cards card text-left text-white bg-dark card-margin-bottom">
-        <div className="card-body">
-          <h4 className="card-title">{post.title}</h4>
-          <div className="row">
-            <div className="col-10">
-              <p className="card-text">
-              <p className="card-text">{post.body}</p>
-                <Link 
-                  to={`/${post.category}/${post.id}`} 
-                  className="card-text">
-                    comments ({`${post.comments ? post.comments.length : 0 }`})
-                </Link>
-              </p>
+      <div>
+        <div className="post-cards card text-left text-white bg-dark card-margin-bottom">
+          <div className="card-body">
+            <h4 className="card-title">{post.title}</h4>
+            <div className="row">
+              <div className="col-10">
+                <p className="card-text">{post.body}</p>
+                  <Link 
+                    to={`/${post.category}/${post.id}`} 
+                    className="card-text">
+                      comments ({`${post.comments ? post.comments.length : 0 }`})
+                  </Link>
+              </div>
+              <div className="col-2">
+                <Vote post={post} path="posts"/>
+              </div>
             </div>
-            <div className="col-2">
-              <Vote post={post} path="posts"/>
+            <hr />
+            <p className="card-text author">Author: {post.author} | Date Posted: {date}</p>
+            
+            <div className="post-buttons float-md-right">
+              <button 
+                onClick={() => { document.getElementById(post.id).style.display = "block" }} 
+                className="btn btn-primary">Edit</button>
+              <button onClick={this.onDeleteClicked.bind(this)} className="btn btn-danger">Delete</button>
             </div>
           </div>
-          <hr />
-          <p className="card-text author">Author: {post.author} | Date Posted: {date}</p>
-          
-          <div className="post-buttons float-md-right">
-            <button onClick={() => history.push('/edit')} className="btn btn-primary">Edit</button>
-            <button onClick={() => history.push('/delete')} className="btn btn-danger">Delete</button>
-          </div>
+        </div>
+        <div id={post.id} className="edit-post-container">
+          <EditPost post={ post } />
         </div>
       </div>
     )
