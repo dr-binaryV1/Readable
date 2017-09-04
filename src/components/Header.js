b5560cba227ebf6a5cea408efe1917e293cacd00
damian.wynter <damian.wynter@particularpresence.com>
2017-09-04 11:37:05 -0500

    fix: Refactor all a tags to Link tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
-export default function Header() {
+function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a className="navbar-brand" href="/">Readable</a>
+    <Link className="navbar-brand" to="/">Readable</Link>
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/add-post">Add Post</a>
+          <Link className="nav-link" to="/add-post">Add Post</Link>
         </li>
       </ul>
     </nav>
   )
 }
+
+export default withRouter(Header);
