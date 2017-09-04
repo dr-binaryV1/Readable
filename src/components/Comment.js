a5b26ca93832bfcc1444842e66934d2923eccca5
damian.wynter <damian.wynter@particularpresence.com>
2017-09-04 11:41:24 -0500

    refactor: Convert Comment component to class component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 import Vote from './Vote';
 
-export default function Comment(props) {
-  const { comment } = props;
-  const date = new Date().toDateString(comment.timestamp);
+class Comment extends Component {
+  onDeleteClicked() {
+    this.props.deleteContent(this.props.comment.id, 'comments');
+  }
 
-  return (
-    <div className="card">
-      <div className="card-body text-white bg-primary">
-        <div className="row">
-          <div className="col-6">
-            <p className="card-text">{comment.body}</p>
+  render() {
+    const { comment } = this.props;
+    const date = new Date().toDateString(comment.timestamp);
+
+    return (
+      <div className="card">
+        <div className="card-body text-white bg-primary">
+          <div className="row">
+            <div className="col-10">
+              <p className="card-text">{comment.body}</p>
+            </div>
+            <div className="col-2">
+              <Vote post={comment} path="comments" />
+            </div>
           </div>
-          <div className="col-6">
-            <Vote post={comment} path="comments" />
+        </div>
+        <div className="card-footer text-white bg-dark">
+          Author: {comment.author} | Date Posted: {date}
+          <div className="float-md-right">
+            <button className="btn btn-primary">Edit</button>
+            <button onClick={this.onDeleteClicked.bind(this)} className="btn btn-danger">Delete</button>
           </div>
         </div>
       </div>
-      <div className="card-footer text-white bg-dark">
-        Author: {comment.author} | Date Posted: {date}
-        <div className="float-md-right">
-          <a href="/edit" className="btn btn-primary">Edit</a>
-          <a href="/delete" className="btn btn-danger">Delete</a>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default connect(null, actions)(Comment);
