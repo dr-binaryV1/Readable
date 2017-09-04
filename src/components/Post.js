6ef6dbddc234d1129efd29a7fe974e0dfa46a867
dr-binaryV1 <developerdamian@gmail.com>
2017-09-04 00:04:36 -0500

    fix: Refactor Post component to container and dispatch delete action when Delete button is clicked

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Vote from './Vote';
 
-export default function Post(props) {
-  const {post} = props;
+class Post extends Component {
+  onDeleteClicked() {
+    this.props.deletePost(this.props.post.id);
+  }
 
-  return (
-    <div className="card text-left text-white bg-dark">
-      <div className="card-body">
-        <h4 className="card-title">{post.title}</h4>
-        <p className="card-text">{post.body}</p>
-        <Link 
-          to={`/${post.category}/${post.id}`} 
-          className="card-text">
-            comment({`${post.comments ? post.comments.length : 0 }`})
-        </Link>
-        <Vote post={post} path="posts"/>
-        <br />
-        <hr />
-        <i><p className="card-text">Author: {post.author}</p></i>
-        
-        <a href="/edit" className="btn btn-primary">Edit</a>
-        <a href="/delete" className="btn btn-danger">Delete</a>
+  render() {
+    const {post} = this.props;
+
+    return (
+      <div className="card text-left text-white bg-dark">
+        <div className="card-body">
+          <h4 className="card-title">{post.title}</h4>
+          <p className="card-text">{post.body}</p>
+          <Link 
+            to={`/${post.category}/${post.id}`} 
+            className="card-text">
+              comments ({`${post.comments ? post.comments.length : 0 }`})
+          </Link>
+          <Vote post={post} path="posts"/>
+          <br />
+          <hr />
+          <i><p className="card-text">Author: {post.author}</p></i>
+          
+          <a href="/edit" className="btn btn-primary">Edit</a>
+          <button
+            onClick={this.onDeleteClicked.bind(this)}
+            className="btn btn-danger">
+              Delete
+          </button>
+        </div>
       </div>
-    </div>
-  )
-} 
+    )
+  }
+}
+
+export default connect(null, actions)(Post);
