cdeaf8cfe6a509ebcab1332f4458bf1f3f7f2d77
dr-binaryV1 <developerdamian@gmail.com>
2017-09-04 21:28:53 -0500

    feat: Add filter function to Component

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import sortBy from 'sort-by';
 
 import * as actions from '../actions';
 import Post from './Post';
@@ -7,13 +8,32 @@ import Post from './Post';
 function PostsList(props) {
   const { posts, category } = props;
 
+  function onFilterChange() {
+    const { posts } = props;
+    const filterControl = document.getElementById("filter-post");
+    const filterBy = filterControl.options[filterControl.selectedIndex].value;
+
+    props.sortPosts(posts.sort(sortBy(filterBy)), filterBy);
+  }
+
   return (
     <div>
-      {posts && category ? posts.filter(post => 
-        {return post.category === category}).map((post) => 
-          post.deleted !== true ? <Post key={post.id} post={post} /> : '') 
-          : posts ? posts.map((post) => 
-              post.deleted !== true ? <Post key={post.id} post={post} /> : '') 
+      <div className="filter-control-container">
+        <label className="float-md-left">Filter By: </label><select
+          onChange={() => onFilterChange()}
+          defaultValue="none"
+          id="filter-post"
+          className="form-control col-2">
+            <option disabled value="none">Filter Post</option>
+            <option value="timestamp">Date</option>
+            <option value="voteScore">Vote Score</option>
+        </select>
+      </div>
+      {posts && category ? posts.filter(post =>
+        {return post.category === category}).map((post) =>
+          post.deleted !== true ? <Post key={post.id} post={post} /> : '')
+          : posts ? posts.map((post) =>
+              post.deleted !== true ? <Post key={post.id} post={post} /> : '')
                 : <h4>There are no Post for { category } at this time.</h4>}
     </div>
   )
@@ -22,7 +42,8 @@ function PostsList(props) {
 
 function mapStateToProps(state) {
   return {
-    posts: state.postData.posts
+    posts: state.postData.posts,
+    sortBy: state.postData.sortBy
   }
 }
 
