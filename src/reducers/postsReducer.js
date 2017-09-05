c5858501f39aa21b38b4505fde19548bfc012db0
dr-binaryV1 <developerdamian@gmail.com>
2017-09-04 21:28:03 -0500

    fix: Add sortBy to state

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,13 +1,17 @@
 import {
-  RECEIVE_POSTS
+  RECEIVE_POSTS,
+  SORT_POSTS
 } from '../actions';
 
 export default function posts(state={}, action) {
-  const { posts } = action;
+  const { posts, method } = action;
 
   switch(action.type) {
     case RECEIVE_POSTS:
       return {...state, posts}
+
+    case SORT_POSTS:
+      return {...state, posts, sortBy: method}
 
     default:
       return state;
